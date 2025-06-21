4d645af669b61ebf71495f5df881f4e65fa29bb1
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-21 12:25:21 +0530

    feat: targetted the input and output select boxes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import "./style.css";
+
+const inpSelect = document.querySelector("#in-countries");
+const outSelect = document.querySelector("#out-countries");
+console.log(inpSelect);
+console.log(outSelect);
