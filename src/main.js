a3b9026de07e59dcb6e971b1e6ab2874e7251c74
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-21 12:30:16 +0530

    feat: fetching currency rates from api

--- src/main.js
+++ src/main.js
@@ -4,3 +4,16 @@ const inpSelect = document.querySelector("#in-countries");
 const outSelect = document.querySelector("#out-countries");
 console.log(inpSelect);
 console.log(outSelect);
+
+async function currencyConverter() {
+  const url = `https://v6.exchangerate-api.com/v6/f5e69d3255659c4b6fefb7ca/latest/USD`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(`Error occured while fetching: ${err}`);
+  }
+}
+
+// currencyConverter();
